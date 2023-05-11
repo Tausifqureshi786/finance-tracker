b96ba3332ce9cbd4559125ed8f66334966a4da92
Tausif Qureshi <59664677+Tausifqureshi786@users.noreply.github.com>
2023-05-11 15:45:48 -0600

    first commit

---
+++ web-app/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>
+          Finance Tracker App
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
