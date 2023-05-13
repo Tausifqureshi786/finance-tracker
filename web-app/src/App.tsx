352b6fc37212a90798b8435f47943aa7b50f2016
Tausifqureshi786 <qamin9333@gmail.com>
2023-05-13 16:46:08 -0600

    calling the express endpoint from react

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,12 +1,31 @@
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+type ApiResponse = {
+  res:String  
+}
 
 function App() {
+  
+  const [apiResponse,setApiResponse] = useState<ApiResponse>({res: ""})
+
+  useEffect(() => {
+    fetch("http://localhost:9000/parsepdf")
+    .then(res => res.text())
+    .then(res => setApiResponse({res: res}))
+    .catch(error => {
+      console.log(error)
+    })
+  },[])
+
   return (
     <div className="App">
       <div>
         <h1>
           Finance Tracker App
         </h1>
+        <p className='App-intre'> {apiResponse? apiResponse.res : ""}</p>
       </div>
     </div>
   );
