788fb090f887231324cfa92912029706b570c264
Tausifqureshi786 <qamin9333@gmail.com>
2023-05-13 19:48:38 -0600

    Allow CORS with express

--- api/index.ts
+++ api/index.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import { normalize } from 'path';
 import {testAPIRouter} from './testAPI'
 import { pdfParseRouter } from './pdfParse';
+import cors from 'cors'
 import path from 'path';
 
 // Initialize the express engine
 const app: express.Application = express();
- 
+
+app.use(cors())
+
 // Take a port 3000 for running server.
 var port = normalize(process.env.PORT || '9000');
  
@@ -17,7 +20,7 @@ const buildPath = path.join(_dirname  , "../web-app/build");
 
 app.use(express.static(buildPath))
 
-app.get("/*", function(req, res){
+app.get("/", function(req, res){
 
     res.sendFile(
         path.join(__dirname, "../web-app/build/index.html"),
