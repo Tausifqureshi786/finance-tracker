d5f92aa405bb09206e8a36e168aa7b964f29599b
Tausifqureshi786 <qamin9333@gmail.com>
2023-05-13 20:09:18 -0600

    cors fix

--- api/index.ts
+++ api/index.ts
@@ -8,7 +8,7 @@ import path from 'path';
 // Initialize the express engine
 const app: express.Application = express();
 
-app.use(cors())
+// app.use(cors())
 
 // Take a port 3000 for running server.
 var port = normalize(process.env.PORT || '9000');
